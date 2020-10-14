5a9efebba9032b76bbf3ade35ff036d53bf466d7
Vinutha-24 <72440023+Vinutha-24@users.noreply.github.com>
2020-10-14 12:26:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+    <title>Credit Management</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body >
+<div id="header">
+       <br>
+       <h1 style=" font-family:sans-serif;  color:black; font-size: 70px  ; "> BASIC BANKING SYSTEM</h1>
+       <br>
+       <h2 style=" font-family:Impact; color:green; font-size: 55px  ; "> SPARKS FOUNDATION INTENSHIP</h2>
+        </div>
+        <div id="section">
+            <table>
+                <tr></tr>
+                <tr>        
+               
+                <div class="css-button" >
+                  <p class="css-button-text">View Users</p>
+                  <div class="css-button-inner">
+                  <a href="getdetail.php" >
+                  <div class="reset-skew">
+                  <p class="css-button-inner-text">View Users</p>
+                </div></a>
+                </div>
+                </div>
+
+                </td>
+                </tr>
+
+                <tr>        
+               <br> <br> <br> <br> <br>
+               <div class="css-button" >
+                 <p class="css-button-text">Credit Transfer</p>
+                 <div class="css-button-inner">
+                 <a href="selectuser.php" >
+                 <div class="reset-skew">
+                 <p class="css-button-inner-text">Credit Transfer</p>
+               </div></a>
+               </div>
+               </div>
+
+               </td>
+               </tr>
+
+            </table>
+    </div>
+
+</body>
+</html>
